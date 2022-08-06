e0b3f345de1ce2407cd10b8b22bacd81287b5628
Leandro Setti <leo.setti@gmail.com>
2022-08-06 11:09:52 -0400

    WIP : form validation

--- Waddame/MainViews/LoginView.swift
+++ Waddame/MainViews/LoginView.swift
@@ -15,12 +15,14 @@ struct LoginView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                LoginText()
-                WelcomeImage()
-                LoginForm()
-                NavView(content: {SignupView()}, text: signuplabel)
-            }.padding()
+            ScrollView{
+                VStack {
+                    LoginText()
+                    WelcomeImage()
+                    LoginForm()
+                    NavView(content: {SignupView()}, text: signuplabel)
+                }.padding()
+            }
         }.environmentObject(userObject)
     }
 }
@@ -56,6 +58,7 @@ struct WelcomeImage: View {
 struct LoginForm: View {
     @EnvironmentObject var viewRouter: ViewRouter
     @EnvironmentObject var userManager: UserLoader
+    @EnvironmentObject var popUpObject: PopUpObject
     
     @State var login: String = ""
     @State var password: String = ""
@@ -63,47 +66,93 @@ struct LoginForm: View {
     var loginlabel: LocalizedStringKey = "login"
     var pwdlabel: LocalizedStringKey = "password"
     
-    var loginError: LocalizedStringKey = "loginError"
-    @State var viewError: Bool = false
+    enum Field: Hashable {
+        case login
+        case password
+    }
+    @FocusState private var focusedField: Field?
+    
+    @State var loginError: LocalizedStringKey = "loginError"
+    @State var viewError1: Bool = false
+    @State var passwordError: LocalizedStringKey = "loginError"
+    @State var viewError2: Bool = false
     
     var body: some View {
-        TextField(loginlabel, text: $login)
-            .padding()
-            .background(lightGreyColor)
-            .cornerRadius(5.0)
-            .padding(.bottom, 10)
-        SecureField(pwdlabel, text: $password)
-                        .padding()
+        VStack{
+            Section{
+                TextField(loginlabel, text: $login)
+                    .padding()
+                    .background(lightGreyColor)
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 10)
+                    .focused($focusedField, equals: .login)
+                    .onSubmit {
+                        if login.count < 2 {
+                            viewError1 = true
+                            loginError = "form_login_error_1"
+                        }else if login.count > 50 {
+                            viewError1 = true
+                            loginError = "form_login_error_2"
+                        }else {
+                            viewError1 = false
+                            focusedField = .password
+                        }
+                    }
+                Text(loginError)
+                    .isHidden(!viewError1)
+                    .frame(maxHeight: viewError1 ? 30 : 0)
+                    .foregroundColor(.red)
+                SecureField(pwdlabel, text: $password)
+                    .padding()
                         .background(lightGreyColor)
-                        .cornerRadius(5.0)
-                        .padding(.bottom, 10)
-        
-        Text(loginError)
-            .isHidden(!viewError)
-        
-        Button(action: {
-            let body: [String: Any] = [
-                "login": login,
-                "password": password
-            ]
-            
-            userManager.loginUser(withObject: body, then: {result in
-                if case .success = result {
-                    DispatchQueue.main.async() {
-                        viewRouter.currentScreen = .main
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 10)
+                    .focused($focusedField, equals: .password)
+                    .onSubmit {
+                        if password.count < 3 {
+                            viewError2 = true
+                            passwordError = "form_password_error_1"
+                        }else if password.count > 30 {
+                            viewError2 = true
+                            passwordError = "form_password_error_2"
+                        }else {
+                            viewError2 = false
+                        }
                     }
+                Text(passwordError)
+                    .isHidden(!viewError2)
+                    .frame(maxHeight: viewError2 ? 30 : 0)
+                    .foregroundColor(.red)
+            }
+            Section{
+                Button(action: {
+                    let body: [String: Any] = [
+                        "login": login,
+                        "password": password
+                    ]
+                    
+                    userManager.loginUser(withObject: body, then: {result in
+                        if case .success = result {
+                            DispatchQueue.main.async() {
+                                viewRouter.currentScreen = .main
+                            }
+                        }
+                        if case .failure = result {
+                            DispatchQueue.main.async() {
+                                popUpObject.title = "popup_error"
+                                popUpObject.message = "loginError"
+                                popUpObject.show.toggle()
+                            }
+                        }
+                    })
+                    
+                }) {
+                    LoginButtonContent()
                 }
-                if case .failure = result {
-                    DispatchQueue.main.async() {
-                        viewError = true
-                    }
-                }
-            })
-            
-        }) {
-            LoginButtonContent()
+            }
+        }.onAppear() {
+            focusedField = .login
         }
-        
     }
 }
 
